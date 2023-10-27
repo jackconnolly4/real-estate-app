ef64ac0c5a985b79d6d94c0a82bab7adc16b9bbf
Jack <jconnolly444@gmail.com>
2023-10-27 14:19:06 -0500

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Home.create(description:"cozy cottage", year_built:1984, square_feet:1300.0, bedrooms: 2, bathrooms: 1.5, floors: 1.5, availability: true, address: "123 Baker Ln, Chicago, IL 55555", price: 600000)
+
+# Home.create(description:"lakeside beauty", year_built:2007, square_feet:2400.0, bedrooms: 4, bathrooms: 2.5, floors: 3, availability: true, address: "123 Buster Ln, Edina, MN 55555", price: 2000000)
